08f7dd9cf412a5fdac5b2f8640780b5f297e9899
Michael Kang <michaelkang85@gmail.com>
2018-01-30 00:24:52 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,49 +16,57 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if board.none? {|i| i == "X" || i == "O"}
-    false
-  else
-    WIN_COMBINATIONS.each do |win_combination|
-      position_1 = win_combination[0]
-      position_2 = win_combination[1]
-      position_3 = win_combination[2]
-      if board[position_1] == "X" && board[position_2] == "X" && board[position_3] == "X" || board[position_1] == "O" && board[position_2] == "O" && board[position_3] == "O"
-        return win_combination
-      end
-    end
-  end
-  false
-end
-
-def full?(board)
-  board.none? {|i| i == " "}
-end
-
-def draw?(board)
-  if won?(board) != false
-    false
-  else
-    full?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
-def over?(board)
-  if full?(board) || won?(board)
-    true
-  else
-    draw?(board)
-  end
-end
-
-
-def winner(board)
-  combo = won?(board)
-  if combo == false
-    nil
-  elsif combo.all? {|index| board[index] == "X"}
-    return "X"
-  else combo.all?{|index| board[index] == "O"}
-    return "O"
-  end
-end
+# def won?(board)
+#   if board.none? {|i| i == "X" || i == "O"}
+#     false
+#   else
+#     WIN_COMBINATIONS.each do |win_combination|
+#       position_1 = win_combination[0]
+#       position_2 = win_combination[1]
+#       position_3 = win_combination[2]
+#       if board[position_1] == "X" && board[position_2] == "X" && board[position_3] == "X" || board[position_1] == "O" && board[position_2] == "O" && board[position_3] == "O"
+#         return win_combination
+#       end
+#     end
+#   end
+#   false
+# end
+# 
+# def full?(board)
+#   board.none? {|i| i == " "}
+# end
+# 
+# def draw?(board)
+#   if won?(board) != false
+#     false
+#   else
+#     full?(board)
+#   end
+# end
+# 
+# def over?(board)
+#   if full?(board) || won?(board)
+#     true
+#   else
+#     draw?(board)
+#   end
+# end
+# 
+# 
+# def winner(board)
+#   combo = won?(board)
+#   if combo == false
+#     nil
+#   elsif combo.all? {|index| board[index] == "X"}
+#     return "X"
+#   elsif combo.all?{|index| board[index] == "O"}
+#     return "O"
+#   end
+# end
